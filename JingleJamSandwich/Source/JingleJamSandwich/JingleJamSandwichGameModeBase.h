072e4642542ad0a319a6da016d8319cb0cceb8d2
isaac-al <isaac-al@users.noreply.github.com>
2019-12-09 21:35:21 +0000

    initjingle

---
+++ JingleJamSandwich/Source/JingleJamSandwich/JingleJamSandwichGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JingleJamSandwichGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JINGLEJAMSANDWICH_API AJingleJamSandwichGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
